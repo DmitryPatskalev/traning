cca7a3b8467ce3df4f42a72c64bfb116ccfd86fa
DmitryPatskalev <dimanord@yandex.ru>
2022-03-01 14:12:28 +0300

    commit

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,18 +1,32 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-const App = () => {
+const prods = [
+  { id: 1, name: "product1", cost: 100 },
+  { id: 2, name: "product2", cost: 200 },
+  { id: 3, name: "product3", cost: 300 },
+];
+function App() {
+  let table = prods.map(function (elem) {
+    return (
+      <tr>
+        <td>
+          {elem.name} {elem.cost}
+        </td>
+      </tr>
+    );
+  });
   return (
-    <div className="App">
-      Hello
-      <ul>
-        <li>1</li>
-        <li>2</li>
-        <li>3</li>
-        <li>4</li>
-      </ul>
-    </div>
+    <table>
+      <thead>
+        <tr>
+          <td>Название</td>
+          <td>Стоимость</td>
+        </tr>
+      </thead>
+      <tbody>{table}</tbody>
+    </table>
   );
-};
+}
 
 export default App;
