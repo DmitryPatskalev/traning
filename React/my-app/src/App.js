829ff4e5b0b637a48e782f019684ff903e343a50
DmitryPatskalev <dimanord@yandex.ru>
2022-03-02 08:30:26 +0300

    commit

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,32 +1,35 @@
 import logo from "./logo.svg";
 import "./App.css";
 
-const prods = [
-  { id: 1, name: "product1", cost: 100 },
-  { id: 2, name: "product2", cost: 200 },
-  { id: 3, name: "product3", cost: 300 },
-];
 function App() {
-  let table = prods.map(function (elem) {
-    return (
-      <tr>
-        <td>
-          {elem.name} {elem.cost}
-        </td>
-      </tr>
-    );
-  });
   return (
-    <table>
-      <thead>
-        <tr>
-          <td>Название</td>
-          <td>Стоимость</td>
-        </tr>
-      </thead>
-      <tbody>{table}</tbody>
-    </table>
+    <div>
+      <Text />
+      <Button />
+    </div>
   );
 }
+
+const Text = () => {
+  return (
+    <>
+      <p>Hello</p>
+      <p>Dima</p>
+    </>
+  );
+};
+
+const Button = () => {
+  let showMe = () => {
+    let i = 1;
+    let count = setInterval(() => {
+      console.log(i++);
+      if (i > 5) {
+        clearInterval(count);
+      }
+    }, 1000);
+  };
+  return <button onClick={showMe}>Click</button>;
+};
 
 export default App;
