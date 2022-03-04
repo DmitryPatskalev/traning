36747e11ab048f8b8453acdbeacdae416efd23ea
DmitryPatskalev <dimanord@yandex.ru>
2022-03-04 15:25:48 +0300

    commit

--- React/my-app/src/components/Profile/Profile.jsx
+++ React/my-app/src/components/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import css from "./Profile.module.css";
 
 const Profile = () => {
   return (
-    <div className={css.content}>
+    <div>
       <div className={css.stadion}>
         <img src="https://media.gettyimages.com/photos/jun-1996-general-aerial-view-of-old-trafford-before-the-european-c-picture-id943836?s=612x612" />
       </div>
