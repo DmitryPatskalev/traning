4bd347757ded5e17a66fdbcc98b97c190b86888f
DmitryPatskalev <dimanord@yandex.ru>
2022-03-09 16:22:40 +0300

    commit

--- React/my-app/src/components/Dialogs/Dialogs.jsx
+++ React/my-app/src/components/Dialogs/Dialogs.jsx
@@ -15,7 +15,7 @@ const Message = (props) => {
 };
 
 const Dialogs = (props) => {
-  let dialogsDate = [
+  let dialogs = [
     { id: 1, name: "Dima" },
     { id: 2, name: "Tanya" },
     { id: 3, name: "Danik" },
@@ -23,27 +23,23 @@ const Dialogs = (props) => {
     { id: 5, name: "Sergei" },
     { id: 6, name: "Ira" },
   ];
-  let messageDate = [
+  let messages = [
     { id: 1, message: "Hello!!" },
     { id: 2, message: "How is it going?" },
     { id: 3, message: "I am fine, thaks!" },
   ];
 
+  let dialogsElements = dialogs.map((dialog) => (
+    <DialogItem name={dialog.name} id={dialog.id} />
+  ));
+  let messagesElements = messages.map((messages) => (
+    <Message message={messages.message} />
+  ));
+
   return (
     <div className={css.dialogs}>
-      <div className={css.dialogsItems}>
-        <DialogItem name={dialogsDate[0].name} id={dialogsDate[0].id} />
-        <DialogItem name={dialogsDate[1].name} id={dialogsDate[1].id} />
-        <DialogItem name={dialogsDate[2].name} id={dialogsDate[2].id} />
-        <DialogItem name={dialogsDate[3].name} id={dialogsDate[3].id} />
-        <DialogItem name={dialogsDate[4].name} id={dialogsDate[4].id} />
-        <DialogItem name={dialogsDate[5].name} id={dialogsDate[5].id} />
-      </div>
-      <div className={css.messages}>
-        <Message message={messageDate[0].message} />
-        <Message message={messageDate[1].message} />
-        <Message message={messageDate[2].message} />
-      </div>
+      <div className={css.dialogsItems}>{dialogsElements}</div>
+      <div className={css.messages}>{messagesElements}</div>
     </div>
   );
 };
--- React/my-app/src/components/Profile/MyPosts/MyPosts.jsx
+++ React/my-app/src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,10 +1,14 @@
 import css from "./MyPosts.module.css";
 import Post from "./Post/Post";
 const MyPosts = () => {
-  const MyPost = [
+  const postDate = [
     { id: 1, message: "How is it going?", like: 15 },
     { id: 2, message: "I am fine, thanks!", like: 20 },
   ];
+  let postsElements = postDate.map((posts) => (
+    <Post message={posts.message} like={posts.like} />
+  ));
+
   return (
     <div className={css.postsBlock}>
       <h3>My posts</h3>
@@ -16,10 +20,7 @@ const MyPosts = () => {
           <button>Add post</button>
         </div>
       </div>
-      <div className={css.posts}>
-        <Post message={MyPost[0].message} like={MyPost[0].like} />
-        <Post message={MyPost[1].message} like={MyPost[1].like} />
-      </div>
+      <div className={css.posts}>{postsElements}</div>
     </div>
   );
 };
