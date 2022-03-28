57948e0437633f9b98f56098a38efb803d5109fd
DmitryPatskalev <dimanord@yandex.ru>
2022-03-28 14:32:30 +0300

    commit

--- other-projects/src/App.js
+++ other-projects/src/App.js
@@ -1,14 +1,16 @@
 // import AppProject_1 from "./Project_1/AppProject_1";
 // import AppProject_2 from "./Project_2/AppProject_2";
+// import AppProject_3 from "./Project_3/AppProject3";
 
-import AppProject_3 from "./Project_3/AppProject3";
+import AppProject_4 from "./Project_4/AppProject_4";
 
 function App() {
   return (
     <div>
       {/* <AppProject_1 /> */}
       {/* <AppProject_2 /> */}
-      <AppProject_3 />
+      {/* <AppProject_3 /> */}
+      <AppProject_4 />
     </div>
   );
 }
