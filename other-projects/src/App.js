de41756860a0eb7d6d6d75e10a400c92df72c338
DmitryPatskalev <dimanord@yandex.ru>
2022-03-29 15:27:27 +0300

    commit

--- other-projects/src/App.js
+++ other-projects/src/App.js
@@ -1,8 +1,9 @@
 // import AppProject_1 from "./Project_1/AppProject_1";
 // import AppProject_2 from "./Project_2/AppProject_2";
 // import AppProject_3 from "./Project_3/AppProject3";
+// import AppProject_4 from "./Project_4/AppProject_4";
 
-import AppProject_4 from "./Project_4/AppProject_4";
+import AppStore from "./Project_5/AppStore";
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       {/* <AppProject_1 /> */}
       {/* <AppProject_2 /> */}
       {/* <AppProject_3 /> */}
-      <AppProject_4 />
+      {/* <AppProject_4 /> */}
+      <AppStore />
     </div>
   );
 }
---
+++ other-projects/src/Project_5/AppStore.jsx
@@ -0,0 +1,9 @@
+import Phone from "./Phone";
+
+export default function AppStore() {
+  return (
+    <div>
+      <Phone />
+    </div>
+  );
+}
