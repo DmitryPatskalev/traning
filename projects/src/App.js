98b3113252be1c0e72ea6d6a2bea6ac11f17d126
DmitryPatskalev <dimanord@yandex.ru>
2022-03-30 20:43:30 +0300

    commit

--- projects/src/App.js
+++ projects/src/App.js
@@ -1,13 +1,16 @@
 // import App1 from "./Project_1/App1";
 // import App2 from "./Project_2/App2";
-import App3 from "./Project_3/App3";
+// import App3 from "./Project_3/App3";
+
+import App4 from "./Project_4/App4";
 
 function App() {
   return (
     <div>
       {/* <App1 /> */}
       {/* <App2 /> */}
-      <App3 />
+      {/* <App3 /> */}
+      <App4 />
     </div>
   );
 }
---
+++ projects/src/Project_4/App4.js
@@ -0,0 +1,20 @@
+import AppTable from "./AppTable";
+import css from "./css.module.css";
+
+function App4() {
+  return (
+    <div className={css.style}>
+      <AppTable />
+    </div>
+  );
+}
+export default App4;
+
+// let teams = [
+//   { id: 1, TEAM: "Manchester Utd", MP: 38, W: 25, D: 8, L: 5 },
+//   { id: 2, TEAM: "Manchester City", MP: 38, W: 23, D: 8, L: 7 },
+//   { id: 3, TEAM: "Liverpool", MP: 38, W: 20, D: 10, L: 8 },
+//   { id: 4, TEAM: "Chelsea", MP: 38, W: 19, D: 12, L: 7 },
+//   { id: 5, TEAM: "Arsenal", MP: 38, W: 18, D: 10, L: 10 },
+//   { id: 6, TEAM: "Tottenham", MP: 38, W: 15, D: 13, L: 10 },
+// ];
