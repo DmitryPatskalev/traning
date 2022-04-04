2b1ab0b7060fe7f1bda902b82b6fcf524f9ffb11
DmitryPatskalev <dimanord@yandex.ru>
2022-04-04 14:33:43 +0300

    commit

--- other-projects/src/App.js
+++ other-projects/src/App.js
@@ -1,26 +1,26 @@
-// import AppProject_1 from "./Project_1/AppProject_1";
-// import AppProject_2 from "./Project_2/AppProject_2";
-// import AppProject_3 from "./Project_3/AppProject3";
-// import AppProject_4 from "./Project_4/AppProject_4";
-// import AppStore from "./Project_5/AppStore";
-// import { BrowserRouter } from "react-router-dom";
-// import MainPage from "./Project_6/Main";
+import AppProject_1 from "./Project_1/AppProject_1";
+import AppProject_2 from "./Project_2/AppProject_2";
+import AppProject_3 from "./Project_3/AppProject3";
+import AppProject_4 from "./Project_4/AppProject_4";
+import AppStore from "./Project_5/AppStore";
+import { BrowserRouter } from "react-router-dom";
+import MainPage from "./Project_6/Main";
 
 import StateRender from "./Project_7/StateRender";
 
 function App() {
   return (
     <div>
-      {/* <BrowserRouter>
+      <BrowserRouter>
         <MainPage />
-      </BrowserRouter> */}
-
+      </BrowserRouter>
       {/* <AppProject_1 /> */}
       {/* <AppProject_2 /> */}
       {/* <AppProject_3 /> */}
       {/* <AppProject_4 /> */}
       {/* <AppStore /> */}
-      <StateRender />
+      {/* <StateRender /> */}
+      {/* <AddElemTable /> */}
     </div>
   );
 }
