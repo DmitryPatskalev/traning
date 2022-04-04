c856c52c9247d5de3ba3cc64ee02d5d25be6b638
DmitryPatskalev <dimanord@yandex.ru>
2022-04-04 14:35:20 +0300

    commit

--- other-projects/src/App.js
+++ other-projects/src/App.js
@@ -1,12 +1,12 @@
-import AppProject_1 from "./Project_1/AppProject_1";
-import AppProject_2 from "./Project_2/AppProject_2";
-import AppProject_3 from "./Project_3/AppProject3";
-import AppProject_4 from "./Project_4/AppProject_4";
-import AppStore from "./Project_5/AppStore";
+// import AppProject_1 from "./Project_1/AppProject_1";
+// import AppProject_2 from "./Project_2/AppProject_2";
+// import AppProject_3 from "./Project_3/AppProject3";
+// import AppProject_4 from "./Project_4/AppProject_4";
+// import AppStore from "./Project_5/AppStore";
 import { BrowserRouter } from "react-router-dom";
 import MainPage from "./Project_6/Main";
 
-import StateRender from "./Project_7/StateRender";
+// import StateRender from "./Project_7/StateRender";
 
 function App() {
   return (
