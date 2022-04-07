219d3f840b860c92f7fa22dc76099e1d941d718d
DmitryPatskalev <dimanord@yandex.ru>
2022-04-07 10:34:32 +0300

    commit

--- other-projects/src/Project_9/AppProject_9.jsx
+++ other-projects/src/Project_9/AppProject_9.jsx
@@ -5,23 +5,24 @@ let listOfProduct = [{ id: 1, brand: "Samsung", model: "S21", price: 1500 }];
 export default function AppProject9() {
   const [value, setValue] = useState(listOfProduct);
   const [par, setPar] = useState();
+
   let result = value.map((elem) => {
     return (
-      <div>
-        <tbody className={css.table}>
-          <td>{elem.id}</td>
-          <td>{elem.brand}</td>
-          <td>{elem.model}</td>
-          <td>{elem.price}</td>
-        </tbody>
-      </div>
+      <tr className={css.table}>
+        <td className={css.par}>{elem.id}</td>
+        <td>{elem.brand}</td>
+        <td>{elem.model}</td>
+        <td className={css.price}>{elem.price}</td>
+        <td>
+          <Sum />
+        </td>
+      </tr>
     );
   });
   return (
     <div>
-      <p onChange={(event) => setValue(event.target.value)}>{result}</p>
-      <button onClick={() => setPar(result)}>BTN</button>
-
+      <p onChange={(event) => setValue(event.target.value)}></p>
+      <button onClick={() => setPar(result)}>Add to Cart</button>
       <Table par={par} />
     </div>
   );
@@ -30,12 +31,27 @@ function Table(props) {
   return (
     <table className={css.table}>
       <tr>
-        <th>№</th>
+        <th className={css.par}>№</th>
         <th>BRAND</th>
         <th>MODEL</th>
-        <th>PRICE</th>
+        <th className={css.price}>PRICE</th>
+        <th>AMOUNT</th>
       </tr>
       {props.par}
     </table>
+  );
+}
+function Sum() {
+  const [input, setInput] = useState(0);
+  return (
+    <div>
+      <button onClick={() => setInput(input - 1)}>-</button>
+      <input
+        className={css.input}
+        value={input}
+        onChange={(event) => setInput(event.target.value)}
+      />
+      <button onClick={() => setInput(input + 1)}>+</button>
+    </div>
   );
 }
