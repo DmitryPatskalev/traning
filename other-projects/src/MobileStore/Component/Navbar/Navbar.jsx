528a82d8dd356ab25124dd324f43013c0e7c4355
DmitryPatskalev <dimanord@yandex.ru>
2022-04-08 15:06:36 +0300

    commit

--- other-projects/src/MobileStore/Component/Navbar/Navbar.jsx
+++ other-projects/src/MobileStore/Component/Navbar/Navbar.jsx
@@ -1,15 +1,16 @@
+import css from "./navbar.module.css";
 import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <div>
-      <div>
+      <div className={css.link}>
         <NavLink to="/main">Главная</NavLink>
       </div>
-      <div>
+      <div className={css.link}>
         <NavLink to="/photo">Фотогалерея</NavLink>
       </div>
-      <div>
+      <div className={css.link}>
         <NavLink to="/cart">Корзина</NavLink>
       </div>
     </div>
