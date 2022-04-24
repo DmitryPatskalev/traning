0053045097eaee7c8e01829422bad6202072bdd3
DmitryPatskalev <dimanord@yandex.ru>
2022-04-24 18:12:41 +0300

    commit

--- projects/src/Project_11/TraningReactInGrid.js
+++ projects/src/Project_11/TraningReactInGrid.js
@@ -4,6 +4,7 @@ import TextInput from "./2_Text_Input";
 import AddToArr from "./3_AddToArr";
 import GetSumOfInput from "./4_SumOfInput";
 import CheckBox from "./5_CheckBox";
+import GetSelect from "./6_Select";
 
 export default function TraningReactInGrid() {
     return <div className={css.main}>
@@ -12,7 +13,7 @@ export default function TraningReactInGrid() {
         <div className={css.odd}><AddToArr/></div>
         <div className={css.even}><GetSumOfInput/></div>
         <div className={css.odd}><CheckBox/></div>
-        <div className={css.even}>6</div>
+        <div className={css.even}><GetSelect/></div>
         <div className={css.odd}>7</div>
         <div className={css.even}>8</div>
         <div className={css.odd}>9</div>
