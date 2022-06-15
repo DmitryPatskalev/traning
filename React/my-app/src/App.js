08f1842caa9130147837f356e4bcde68c89c2849
DmitryPatskalev <dimanord@yandex.ru>
2022-06-15 08:44:54 +0300

    commit

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -4,8 +4,9 @@ import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import { Routes, Route } from "react-router-dom";
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
+import Users from "./components/Users/Users";
 
-function App(props) {
+function App() {
   return (
 
     <div className="app-wrapper">
@@ -15,6 +16,8 @@ function App(props) {
         <Routes>
           <Route path="/dialogs/*" element={<DialogsContainer />} />
           <Route path="/profile" element={<Profile />} />
+            <Route path='/users' element={<Users/>}/>
+
         </Routes>
       </div>
     </div>
